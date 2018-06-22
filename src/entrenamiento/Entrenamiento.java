1de6fd31dce80042e720b95509df01ec3f54ecb1
Jonathan Buzzetti <buzzetti.jp@gmail.com>
2018-06-21 23:58:08 -0300

    empiezo parte de entrenamiento

--- src/entrenamiento/Entrenamiento.java
+++ src/entrenamiento/Entrenamiento.java
@@ -1,11 +1,28 @@
 package entrenamiento;
 
+import java.io.File;
+
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
 public class Entrenamiento {
 	
-	public static void run(String directorioImagenes) {
+	public static void run() {
 
+		System.out.println("Generando ofertas.info");
+		String directorioPgmPositivas = Config.getInstance().getConfig("imgPosPath") + "/pgm";
 		
+		File[] imagenes = new File(directorioPgmPositivas).listFiles();
+		
+		for (int i = 0; i < imagenes.length; i++) {
+			Mat imagen = Imgcodecs.imread(imagenes[i].getAbsolutePath());
+			
+			int ancho = imagen.width();
+			int alto = imagen.height();
 
+			String filename = "/recursos/ofertas.info";
+			Imgcodecs.imwrite(filename, imagen);
+		}
 	}
 
 
